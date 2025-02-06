e6f052e620e700db949250c20747afee81b1b110
Carlos Ramirez <carlosdanielrt@gmail.com>
2025-02-06 10:58:42 -0400

    [ADD] implement the main file to serve the app in the localhosst through port 3000 reloading on python changes using uvicorn.

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.routes import payroll
+
+app = FastAPI(
+    title="Paystub Notifier API",
+    description="API para procesar planillas, generar recibos de pago y enviarlos por correo electrónico.",
+    version="1.0.0"
+)
+
+app.include_router(payroll.router, prefix="/api", tags=["Payroll"])
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="127.0.0.1", port=3000, reload=True)
